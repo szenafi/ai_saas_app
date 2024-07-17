ccc21a34df78ff897ac297631e50c281562c951f
ZENAFI Saïd <s.zenafi@mairie-laciotat.fr>
2024-07-17 16:18:10 +0200

    first commit

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -74,7 +74,7 @@ const MobileNav = () => {
 
         <SignedOut>
             <Button asChild className="button bg-purple-gradient bg-cover">
-              <Link href="/sign-in">Login</Link>
+              <Link href="/sign-in">se connecter</Link>
             </Button>
           </SignedOut>
       </nav>
